8a38acc4278485bea387fc51a909f1f4563f0849
manu <j4ckdaw@protonmail.com>
2022-12-08 14:03:36 +0000

    cleanup

--- code/8.hs
+++ code/8.hs
@@ -1,11 +1,10 @@
 import Data.Array (Array, array, range, bounds, (!))
 
 createGrid :: [String] -> Array (Int, Int) Int
-createGrid input = grid
+createGrid input = array ((0,0), (h,w)) [ ((i,j), getTree (i,j)) | (i,j) <- range ((0,0), (h,w)) ]
     where
         w = length (input !! 0) - 1
         h = length input - 1
-        grid = array ((0,0), (h,w)) [((i,j), getTree (i,j)) | (i,j) <- range ((0,0), (h,w))]
         getTree (i,j) = read [(input !! i) !! j]::Int
 
 visible :: Array (Int, Int) Int -> (Int, Int) -> Bool
@@ -13,10 +12,10 @@ visible grid (i,j) = up || down || left || right
     where
         (h,w) = snd $ bounds grid
         height = grid ! (i, j)
-        up     = all (<height) [grid ! (k,j) | k <- [0..i-1]]
-        down   = all (<height) [grid ! (k,j) | k <- [i+1..h]]
-        left   = all (<height) [grid ! (i,k) | k <- [0..j-1]]
-        right  = all (<height) [grid ! (i,k) | k <- [j+1..w]]
+        up     = all (<height) [ grid ! (k,j) | k <- [0..i-1] ]
+        down   = all (<height) [ grid ! (k,j) | k <- [i+1..h] ]
+        left   = all (<height) [ grid ! (i,k) | k <- [0..j-1] ]
+        right  = all (<height) [ grid ! (i,k) | k <- [j+1..w] ] 
 
 -- also takes the first element that fails the predicate (i.e the tree that blocks the view)
 takeWhile' :: (a -> Bool) -> [a] -> [a]
@@ -28,21 +27,19 @@ scenicScore grid (i,j) = up * down * left * right
     where
         (h,w) = snd $ bounds grid
         height = grid ! (i,j)
-        up     = length $ takeWhile' (<height) $ reverse [grid ! (k,j) | k <- [0..i-1]]
-        down   = length $ takeWhile' (<height)           [grid ! (k,j) | k <- [i+1..h]]
-        left   = length $ takeWhile' (<height) $ reverse [grid ! (i,k) | k <- [0..j-1]]
-        right  = length $ takeWhile' (<height)           [grid ! (i,k) | k <- [j+1..w]]
+        up     = length $ takeWhile' (<height) $ reverse [ grid ! (k,j) | k <- [0..i-1] ]
+        down   = length $ takeWhile' (<height)           [ grid ! (k,j) | k <- [i+1..h] ]
+        left   = length $ takeWhile' (<height) $ reverse [ grid ! (i,k) | k <- [0..j-1] ]
+        right  = length $ takeWhile' (<height)           [ grid ! (i,k) | k <- [j+1..w] ]
 
 
 solveA :: [String] -> Int
-solveA input = length $ filter id [vis ! (i,j) | (i,j) <- range $ bounds vis]
-    where grid   = createGrid input
-          vis    = array (bounds grid) [((i,j), visible grid (i,j)) | (i,j) <- range (bounds grid)]
+solveA input = length $ filter id [ visible grid (i,j) | (i,j) <- range (bounds grid) ]
+    where grid = createGrid input
 
 solveB :: [String] -> Int
-solveB input = maximum [scores ! (i,j) | (i,j) <- range $ bounds scores]
-    where grid   = createGrid input
-          scores = array (bounds grid) [((i,j), scenicScore grid (i,j)) | (i,j) <- range (bounds grid)]
+solveB input = maximum [ scenicScore grid (i,j) | (i,j) <- range (bounds grid) ]
+    where grid = createGrid input
 
 main = do
     input <- readFile "inputs/8.txt"
